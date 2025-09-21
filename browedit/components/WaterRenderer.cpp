d975216a15b576bc5226acfbc1626e485a25ff19
2br <2br@users.noreply.github.com>
2025-09-21 11:43:18 -0400

    discard for now

--- browedit/components/WaterRenderer.cpp
+++ browedit/components/WaterRenderer.cpp
@@ -162,7 +162,7 @@ void WaterRenderer::reloadTextures()
 		for (int x = 0; x < rsw->water.splitWidth; x++) {
 			for (int i = 0; i < 32; i++) {
 				char buf[128];
-				sprintf_s(buf, 128, "data/texture/water/water%i%02i%s", rsw->water.zones[x][y].type, 0, ".png");
+				sprintf_s(buf, 128, "data/texture/water/water000.png");
 
 				int index = 32 * (y * rsw->water.splitWidth + x) + i;
 				if (index >= textures.size()) {
